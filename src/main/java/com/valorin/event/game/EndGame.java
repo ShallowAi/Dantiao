68ba19d9935c63b9b8e7f77a62d98df7f395a52d
ShallowAi <1402320551@qq.com>
2020-08-07 21:36:37 +0800

    新增限制模式

--- src/main/java/com/valorin/event/game/EndGame.java
+++ src/main/java/com/valorin/event/game/EndGame.java
@@ -3,10 +3,14 @@ package com.valorin.event.game;
 import static com.valorin.configuration.languagefile.MessageSender.sm;
 
 import org.bukkit.Bukkit;
+import org.bukkit.attribute.Attribute;
+import org.bukkit.entity.EntityType;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.EventPriority;
 import org.bukkit.event.Listener;
+import org.bukkit.event.entity.EntityDamageByEntityEvent;
+import org.bukkit.event.entity.EntityDamageEvent;
 import org.bukkit.event.entity.PlayerDeathEvent;
 import org.bukkit.event.player.PlayerQuitEvent;
 
@@ -16,7 +20,46 @@ import com.valorin.arenas.ArenasManager;
 import com.valorin.arenas.Finisher;
 
 public class EndGame implements Listener {
-	//一方选手死亡，无论因素，判定“非平局结束比赛”
+	// 限制模式：一方选手血量低于50%，无论因素，判定“非平局结束比赛”
+	@EventHandler(priority=EventPriority.HIGHEST)
+	public void onDTDmgHalf(EntityDamageEvent e) {
+		// 判断是否为该模式
+		// some code ...
+			if (e.getEntity().getType() == EntityType.PLAYER) {
+				Player p = (Player) e.getEntity();
+				if (p.getHealth() <= p.getAttribute(Attribute.GENERIC_MAX_HEALTH).getValue() / 2) {
+					String pn = p.getName();
+					ArenasManager ah = Dantiao.getInstance().getArenasHandler();
+					if (ah.isPlayerBusy(pn)) { //属于比赛玩家，对方取胜
+						String an = ah.getPlayerOfArena(pn);
+						Arena a = ah.getArena(an);
+						String winner = a.getTheOtherPlayer(pn);
+						Finisher.normalEnd(an, winner, pn, false);
+					}
+				}
+			}
+	}
+/*
+	// 一击决胜模式：一方选手被击中，无论因素，判定“非平局结束比赛”
+	@EventHandler(priority=EventPriority.HIGHEST)
+	public void onDTDmg(EntityDamageEvent e) {
+		// 判断是否为该模式
+		// some code ...
+			if (e.getEntity().getType() == EntityType.PLAYER && e.getFinalDamage() >= 0) {
+				Player p = (Player) e.getEntity();
+				String pn = p.getName();
+				ArenasManager ah = Dantiao.getInstance().getArenasHandler();
+				if (ah.isPlayerBusy(pn)) { //属于比赛玩家，对方取胜
+					String an = ah.getPlayerOfArena(pn);
+					Arena a = ah.getArena(an);
+					String winner = a.getTheOtherPlayer(pn);
+					Finisher.normalEnd(an, winner, pn, false);
+				}
+			}
+	}
+	
+ */
+	// 普通模式：一方选手死亡，无论因素，判定“非平局结束比赛”
 	@EventHandler(priority=EventPriority.HIGHEST)
     public void onDTDeath(PlayerDeathEvent e) {
 	  Player p = e.getEntity();
@@ -29,7 +72,8 @@ public class EndGame implements Listener {
 		Finisher.normalEnd(an, winner, pn, false);
 	  }
     }
-	//一方玩家下线，无论因素，判定“非平局结束比赛”
+
+	// 一方玩家下线，无论因素，判定“非平局结束比赛”
 	@EventHandler
     public void onDTLeaveGame(PlayerQuitEvent e) {
 	  Player p = e.getPlayer();
